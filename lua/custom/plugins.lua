44f72e6e924bab2eb1991fddafb9e694c54f0673
Andreas Corneliussen <aco@skytem.com>
2023-12-17 20:38:04 +0100

    Added tmux nvim navigation

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -89,6 +89,25 @@ local plugins = {
       map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
     end,
   },
+  {
+  "alexghergh/nvim-tmux-navigation",
+    event = "VeryLazy",
+    config = function()
+      local nvim_tmux_nav = require("nvim-tmux-navigation")
+      nvim_tmux_nav.setup({
+        disable_when_zoomed = true,
+        -- defaults to false
+        keybindings = {
+          left = "<C-h>",
+          down = "<C-j>",
+          up = "<C-k>",
+          right = "<C-l>",
+          last_active = "<C-\\>",
+          next = "<C-Space>",
+        },
+      })
+    end,
+  }
 }
 
   --dTo make a plugin not be loaded
